f62fdbc4816bd59c4b1a1bee206cc23eec4b27fc
BereketMelese <bereketmeles00@gmail.com>
2024-05-09 00:24:53 +0300

    try

--- script.js
+++
@@ -1,9 +0,0 @@
-const btnEl = document.getElementById("convert-btn");
-const outEl = document.getElementById("output");
-const closeEl = document.querySelector(".fa-solid")
-btnEl.addEventListener("click", ()=>{
-    outEl.classList.toggle("hidden")
-})
-closeEl.addEventListener("click", ()=> {
-    outEl.classList.toggle("hidden")
-})
