9b980c32bbc7e2e892097b8637ea9fc4f48f3126
BereketMelese <bereketmeles00@gmail.com>
2024-05-12 17:07:15 +0300

    initial commit

--- Telephone-number-validator/script.js
+++ Telephone-number-validator/script.js
@@ -1,31 +1,44 @@
 const inputEl = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const cleatBtn = document.getElementById("clear-btn");
-const resultEl = document.getElementById("result-div");
+const resultEl = document.getElementById("results-div");
 const inputDivEl = document.getElementById("input-div");
+const bodyEl = document.querySelector(".main")
 const validCol = "#00FF00";
 const invalidCol = "#FF0000";
 const original = "#99d8be";
 
-checkBtn.addEventListener("click", validatePhoneNumber(inputVal));
+checkBtn.addEventListener("click", () => {
+    resultEl.classList.remove("hidden");
+    validatePhoneNumber();
+});
+inputEl.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        resultEl.classList.remove("hidden");
+        validatePhoneNumber();
+    }
+})
 cleatBtn.addEventListener("click", clearResults);
 
 
 function validatePhoneNumber()  {
-    const phoneNumber = inputEl.value.trim();
 
+    const phoneNumber = inputEl.value.trim();
     const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
 
     if (phoneNumber === '') {
         alert("Please provide a phone number");
+        inputDivEl.style.borderColor = invalidCol;
         return;
     }
 
-    resultEl.textContent = phoneRegex.test(phoneNumber) ? (`Valid US number: ${phoneNumber}`, inputDivEl.style.borderColor = validCol, resultEl.style.borderColor = validCol)  : (`Invalid US number: ${phoneNumber}`, inputDivEl.style.borderColor = invalidCol, resultEl.style.borderColor = invalidColvalidCol)
+    phoneRegex.test(phoneNumber) ? (resultEl.textContent = `Valid US number: ${phoneNumber}`, inputDivEl.style.borderColor = validCol, resultEl.style.borderColor = validCol)  : (resultEl.textContent = `Invalid US number: ${phoneNumber}`, inputDivEl.style.borderColor = invalidCol, resultEl.style.borderColor = invalidCol)
 };
 
 function clearResults() {
     resultEl.textContent = '';
+    inputEl.value = '';
     inputDivEl.style.borderColor = original;
+    resultEl.classList.add("hidden");
     resultEl.style.borderColor = original;
 };
